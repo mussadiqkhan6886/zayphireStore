bedfeeffefacc3db6fd97b3c22095807eb27a5fe
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-20 18:36:25 +0500

    fixed button issue

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -7,12 +7,12 @@ const Items = ({id, name, img, price}) => {
   return (
     <>
     <div data-aos="fade-up" data-aos-delay="100" className='p-3 flex flex-col items-center'>
-      <Link to={`/product/${id}`}> <img onClick={window.scrollTo(0,0)} src={img} className='w-[250px] mb-2 hover:scale-105 cursor-pointer transition-all' alt="image item" /></Link>
+      <Link to={`/product/${id}`}> <img onClick={() => window.scrollTo(0,0)} src={img} className='w-[250px] mb-2 hover:scale-105 cursor-pointer transition-all' alt="image item" /></Link>
       <p className='text-center px-7'>{name}</p>
       <div className='flex gap-5 mt-1'>
          <p className='font-semibold'>PKR {price}</p>
       </div>
-     <button onClick={() => addToCart(id)} className='cursor-pointer text-black border-black border px-7 py-3 text-[14px]'>ADD TO CART</button>
+     <button onClick={() => addToCart(id)} className='hover:bg-gray-300 mt-2 cursor-pointer text-black border-black border px-7 py-3 text-[14px]'>ADD TO CART</button>
     </div>
      </>
   )
